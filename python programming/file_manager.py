09dccbe663f0b3209434253c626f32e7c4d7572e
prajwal140805 <prajwalmagishettar2005@gmail.com>
2025-06-13 03:54:34 +0530

    3:54/13062025

--- python programming/file_manager.py
+++ python programming/file_manager.py
@@ -5,13 +5,19 @@ b=31
 c=10
 d=31
 
-for i in range(a,b):
-    os.path.join('python programming', f'test{i}.cpp')
-    with open( os.path.join('python programming', f'test{i}.cpp'), 'w') as file:
-        file.write( "")   
+# for i in range(a,b):
+#     os.path.join('python programming', f'test{i}.cpp')
+#     with open( os.path.join('python programming', f'test{i}.cpp'), 'w') as file:
+#         file.write( "")   
+        
+        
+        
 #this is to delete the files created above
 # for i in range(c,d):
 #     os.remove (f'c++programming/test{i}.cpp') 
+
+
+
 
 # with open ('python programming/test3.py', 'r') as f:
 #     f.seek(0)
