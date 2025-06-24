9ad4581d8a43e6fab0f7a0ed100f30bd3e8c0a38
prajwal140805 <prajwalmagishettar2005@gmail.com>
2025-06-25 02:20:23 +0530

    02:03/250625

--- python programming/beeper.py
+++ python programming/beeper.py
@@ -1,4 +1,29 @@
 import winsound
+import os
 
-winsound.Beep(5000,700)
-print()      
+os.system('cls')
+print("The Healing Vibe")
+winsound.Beep(432  ,10000)
+os.system('cls')
+
+print("The Love Frequency")
+winsound.Beep(528  ,10000)
+os.system('cls')
+
+print("Letting Go")
+winsound.Beep(396  ,10000)
+os.system('cls')
+
+print("Detoxifying, cleansing, inner awakening")
+winsound.Beep(741  ,10000)
+os.system('cls')
+
+print("Improves relationships, connects heart to mind")
+winsound.Beep(639  ,10000)
+os.system('cls')
+
+print("	Pain relief, physical tension release")
+winsound.Beep(174  ,10000)
+os.system('cls')
+
+     
