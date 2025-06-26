a75b8fba21a74f2ce42a226c1c79bbb695b6ab1d
prajwal140805 <prajwalmagishettar2005@gmail.com>
2025-06-27 00:49:44 +0530

    02:03/250625

--- python programming/beeper.py
+++ python programming/beeper.py
@@ -1,29 +1,39 @@
 import winsound
 import os
+while True:
 
-os.system('cls')
-print("The Healing Vibe")
-winsound.Beep(432  ,10000)
-os.system('cls')
+    print("	Pain relief, physical tension release")
+    winsound.Beep(32767,10000000)
+    os.system('cls')
+    # print("	Pain relief, physical tension release")
+    # winsound.Beep(174  ,5000)
+    # os.system('cls')
+    # print("	Pain relief, physical tension release")
+    # winsound.Beep(274  ,5000)
+    # os.system('cls')
+    # print("Letting Go")
+    # winsound.Beep(396  ,5000)
+    # os.system('cls')
+    
+    # os.system('cls')
+    # print("The Healing Vibe")
+    # winsound.Beep(432  ,5000)
+    # os.system('cls')
 
-print("The Love Frequency")
-winsound.Beep(528  ,10000)
-os.system('cls')
+    # print("The Love Frequency")
+    # winsound.Beep(490  ,5000)
+    # os.system('cls')
+    # print("The Love Frequency")
+    # winsound.Beep(528  ,5000)
+    # os.system('cls')
 
-print("Letting Go")
-winsound.Beep(396  ,10000)
-os.system('cls')
+    # print("Improves relationships, connects heart to mind")
+    # winsound.Beep(639  ,5000)
+    # os.system('cls')
 
-print("Detoxifying, cleansing, inner awakening")
-winsound.Beep(741  ,10000)
-os.system('cls')
+    # print("Detoxifying, cleansing, inner awakening")
+    # winsound.Beep(741  ,5000)
+    # os.system('cls')
 
-print("Improves relationships, connects heart to mind")
-winsound.Beep(639  ,10000)
-os.system('cls')
-
-print("	Pain relief, physical tension release")
-winsound.Beep(174  ,10000)
-os.system('cls')
 
      
