f1f741929622b48826f6808e729af9f9a4c9bc71
prajwal140805 <prajwalmagishettar2005@gmail.com>
2025-07-08 23:15:02 +0530

    0807

--- codeverse/c++programming/calculator.cpp
+++ codeverse/c++programming/calculator.cpp
@@ -3,43 +3,47 @@
 using namespace std;
 int main()
 {
-    int x, y, result;
+    int x, y,result;
     char op;
-    cout << "Enter the first number:\n";
+    system("cls");
+    cout << "Enter:\n";
     cin >> x;
-    cout << "enter the operator among +,-,*,/,%(modulo oprt),p(power):" << '\n';
     cin >> op;
-    cout << "Enter the second number:\n";
     cin >> y;
 
     switch (op)
     {
     case '+':
         result = x + y;
-        cout << "The result is :" << result;
+        cout << x<<'+'<<y<<'=' << result;
         break;
     case '-':
         result = x - y;
-        cout << "The result is :" << result;
+        cout << x<<'-'<<y<<'=' << result;
         break;
     case '*':
         result = x * y;
-        cout << "The result is :" << result;
+        cout << x<<'*'<<y<<'=' << result;
         break;
     case '/':
         result = x / y;
-        cout << "The result is :" << result;
+        cout << x<<'/'<<y<<'=' << result;
         break;
     case 'p':
         result = pow(x, y);
-        cout << "The result is :" << result;
+        cout << '('<<x<<')'<<'^'<<y<<'=' << result;
         break;
     case '%':
         result = x % y;
-        cout << "The result is :" << result;
+        cout  << x<<'%'<<y<<'=' << result;
         break;
     default:
         cout << "enter the valid operator";
     }
+    cout << "Calculation done! Press Enter to clear and continue...\n";
+cin.ignore();
+cin.get();
+system("cls");
+
     return 0;
 }
